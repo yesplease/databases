13c09f23a7365b52ec54f2ede63c7caaefda1f7f
yesplease <bradley.heinz@gmail.com>
2015-02-27 17:30:34 -0800

    implement sql schema design

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -5,7 +5,7 @@ $(function() {
   app = {
 //TODO: The current 'addFriend' function just adds the class 'friend'
 //to all messages sent by the user
-    server: 'localhost:3000',
+    server: 'localhost:3000/classes/',
     username: 'anonymous',
     roomname: 'lobby',
     lastMessageId: 0,
@@ -60,7 +60,7 @@ $(function() {
         url: app.server,
         type: 'GET',
         contentType: 'application/json',
-        data: { order: '-createdAt'},
+        // data: { order: '-createdAt'},
         success: function(data) {
           console.log('chatterbox: Messages fetched');
 
--- server/schema.sql
+++ server/schema.sql
@@ -1,10 +1,43 @@
+DROP DATABASE IF EXISTS chat;
+
 CREATE DATABASE chat;
 
 USE chat;
 
+DROP TABLE IF EXISTS messages;
 CREATE TABLE messages (
-  /* Describe your table here.*/
+  id INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  text VARCHAR(255) NOT NULL DEFAULT 'NULL',
+  timestamp TIMESTAMP NULL DEFAULT NULL,
+  roomName VARCHAR(20) NULL DEFAULT NULL,
+  username VARCHAR(20) NULL DEFAULT NULL,
+  PRIMARY KEY (id)
 );
+
+
+
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+  username VARCHAR(20) NULL DEFAULT NULL,
+  PRIMARY KEY (username)
+);
+
+
+
+DROP TABLE IF EXISTS rooms;
+
+CREATE TABLE rooms (
+  roomName VARCHAR(20) NULL DEFAULT NULL,
+  PRIMARY KEY (roomName)
+);
+
+ALTER TABLE messages ADD FOREIGN KEY (roomName) REFERENCES rooms (roomName);
+ALTER TABLE messages ADD FOREIGN KEY (username) REFERENCES users (username);
+
+
+
+
 
 /* Create other tables and define schemas for them here! */
 
